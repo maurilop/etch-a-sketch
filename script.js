858e973feca371d4429f2c4fe6cc2550655c1e20
Mauricio López <maurilop92@gmail.com>
2023-09-04 20:55:31 -0300

    Finished everything, didn't commit frequently :(

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
-//function createGrid() {}
+const gridContainer = document.getElementById("gridContainer");
+const gridSizeBtn = document.getElementById("gridSizeBtn");
+const clearGridBtn = document.getElementById("clearGridBtn");
 
-function createTile(d) {
-  let tile = document.createElement("div");
-  tile.classList.add("tile");
-  document.querySelector(".tile").style.width = d;
-  document.querySelector(".tile").style.height = d;
-  document.querySelector(".tile").style.backgroundColor = "red";
-  document.body.appendChild(tile);
+let gridSize;
+
+function createGrid(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    createTileRow();
+    for (let j = 0; j < gridSize; j++) {
+      createTile();
+    }
+  }
 }
 
-//createTile(100);
+function createTileRow() {
+  let tileRow = document.createElement("div");
+  tileRow.classList.add("tileRow");
+  tileRow.style.backgroundColor = "white";
+  tileRow.style.display = "flex";
+  tileRow.style.flexGrow = "1";
+  tileRow.style.flexDirection = "row";
+  gridContainer.append(tileRow);
+}
 
-let tile = document.createElement("div");
-/* tile.classList.add("tile");
-document.querySelector(".tile").style.width = 100;
-document.querySelector(".tile").style.height = 100;
-document.querySelector(".tile").style.backgroundColor = "red"; */
-document.body.appendChild(tile);
+function createTile() {
+  let tile = document.createElement("div");
+  tile.classList.add("tile");
+  tile.style.display = "flex";
+  tile.style.flexGrow = "1";
+  tile.style.backgroundColor = "white";
+
+  tile.addEventListener("mouseover", function () {
+    this.style.backgroundColor = "black";
+    this.style.opacity = (parseFloat(this.style.opacity) || 0) + 0.2;
+  });
+
+  gridContainer.lastChild.append(tile);
+}
+
+createGrid(50);
+
+gridSizeBtn.addEventListener("click", function () {
+  do {
+    gridSize = prompt("What size should the grid be? Max: 100");
+  } while (gridSize > 100 || gridSize <= 0);
+  clearGrid();
+  createGrid(gridSize);
+});
+
+function clearGrid() {
+  gridContainer.innerHTML = "";
+}
+
+clearGridBtn.addEventListener("click", function () {
+  clearGrid();
+  createGrid(50);
+});
